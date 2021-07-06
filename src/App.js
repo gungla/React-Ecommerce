9dd28762b858bf4c6ccd0cee85e0b4a2628afded
gungla <gglahn@gmail.com>
2021-07-06 15:00:31 -0300

    cambio a className

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
              <h4>Las ofertas de la semana</h4>
           </div>
         </div>
